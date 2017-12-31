8be2eb5d229c1baaded1375843044afedd754ec1
ss68974 <sushanta.samantaray@gmail.com>
2018-01-01 00:00:26 +0530

    unit testing completed #patch

--- pyspark_cicd/pyspark_cicd/testspark.py
+++ pyspark_cicd/pyspark_cicd/testspark.py
@@ -1,29 +1,28 @@
 import os
 import sys
+from operator import add
 
 from pyspark import SparkContext, SparkConf
 
-def do_word_counts():
+def do_word_counts(lines):
     """ count of words in an rdd of sushanta lines """
 
-    sc = SparkContext()
-
-    print("first spark program")
-
-    text_file = sc.textFile("D:\Sample files\\wordcount.txt")
-
-    counts = text_file.flatMap(lambda line: line.split(" ")) \
-        .map(lambda word: (word, 1)) \
-        .reduceByKey(lambda a, b: a + b)
+    counts = (lines.flatMap(lambda x: x.split())
+        .map(lambda x: (x, 1))
+        .reduceByKey(add)
+        )
 
     results = {word: count for word, count in counts.collect()}
-
-    output = counts.collect()
-    for (word, count) in output:
-        print("%s: %i" % (word, count))
-
     return results
+
 
 if __name__ == "__main__":
 
-    do_word_counts()
+#    if len(sys.argv) != 2:
+#        sys.exit("Usage: wordcount file}")
+
+    sc = SparkContext(appName="PythonWordCount")
+#    lines = sc.textFile(sys.argv[1], 1)
+    lines = sc.textFile("D:\Sample Files\wordcount.txt", 1)
+
+    print(do_word_counts(lines))
