4a12406a488e28c7fdf11d3660d97e4f64b9d94d
ss68974 <sushanta.samantaray@gmail.com>
2017-12-31 10:12:14 +0530

    word count example #patch

--- testspark.py
+++ testspark.py
@@ -1,3 +1,17 @@
-from pyspark import SparkContext
+import os
+import sys
 
-print("welcome to spark")
+from pyspark import SparkContext, SparkConf
+
+sc = SparkContext()
+
+print('First Spark Program')
+
+text_file = sc.textFile("D:\Sample files\\wordcount.txt")
+counts = text_file.flatMap(lambda line: line.split(" ")) \
+             .map(lambda word: (word, 1)) \
+             .reduceByKey(lambda a, b: a + b)
+for x in counts.collect():
+    print(x)
+
+
